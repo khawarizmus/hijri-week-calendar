e4af424d1b3f3a377bd0cd9eda1283b4e03870b4
Mohsen Alyafei <51336583+MohsenAlyafei@users.noreply.github.com>
2024-01-02 11:14:35 +0300

    Update core.test.ts
    Calendar name can be passed directly as last function parameter

--- test/core.test.ts
+++ test/core.test.ts
@@ -4,8 +4,8 @@ import { hwcToString, toHWCDate, totalHWCWeeks } from '../src/core'
 describe('calculations from Hijri to Hijri Week Calendar', () => {
   it('should output the right Hijri Week Dates', () => {
     expect(hwcToString(toHWCDate(1445, 6, 14))).toEqual('1445-W23-5')
-    expect(hwcToString(toHWCDate(1445, 6, 14, { fromCal: 'islamic-civil' }))).toEqual('1445-W23-5')
-    expect(hwcToString(toHWCDate(1445, 6, 14, { fromCal: 'islamic-tbla' }))).toEqual('1445-W24-4')
+    expect(hwcToString(toHWCDate(1445, 6, 14, 'islamic-civil'))).toEqual('1445-W23-5')
+    expect(hwcToString(toHWCDate(1445, 6, 14, 'islamic-tbla'))).toEqual('1445-W24-4')
     expect(hwcToString(toHWCDate(1, 1, 1))).toEqual('0-W51-7')
     expect(hwcToString(toHWCDate(1444, 2, 2))).toEqual('1444-W05-3')
     // Negative Hijri Year
@@ -17,12 +17,12 @@ describe('calculations from Hijri to Hijri Week Calendar', () => {
     expect(totalHWCWeeks(1445)).toEqual(50)
     expect(totalHWCWeeks(1446)).toEqual(51)
 
-    expect(totalHWCWeeks(1444, { fromCal: 'islamic-civil' })).toEqual(50)
-    expect(totalHWCWeeks(1445, { fromCal: 'islamic-civil' })).toEqual(50)
-    expect(totalHWCWeeks(1446, { fromCal: 'islamic-civil' })).toEqual(51)
+    expect(totalHWCWeeks(1444, 'islamic-civil')).toEqual(50)
+    expect(totalHWCWeeks(1445, 'islamic-civil')).toEqual(50)
+    expect(totalHWCWeeks(1446, 'islamic-civil')).toEqual(51)
 
-    expect(totalHWCWeeks(1444, { fromCal: 'islamic-tbla' })).toEqual(50)
-    expect(totalHWCWeeks(1445, { fromCal: 'islamic-tbla' })).toEqual(50)
-    expect(totalHWCWeeks(1446, { fromCal: 'islamic-tbla' })).toEqual(51)
+    expect(totalHWCWeeks(1444, 'islamic-tbla')).toEqual(50)
+    expect(totalHWCWeeks(1445, 'islamic-tbla')).toEqual(50)
+    expect(totalHWCWeeks(1446, 'islamic-tbla')).toEqual(51)
   })
 })
